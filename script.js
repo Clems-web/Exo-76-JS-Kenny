60f430db8800ff7158c0577d5529ae45ea664c7a
Clems-web <clement.servais1@gmail.com>
2020-12-04 16:02:32 +0100

    II fait

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-
+let viewport = document.getElementById("viewport");
+let style = window.getComputedStyle(viewport);
 
 document.getElementById('up').addEventListener('click',function() {
     let t = parseInt(document.getElementById('kenny').style.top);
     t = t - 10;
+    if(t > 0) {
+        document.getElementById('kenny').style.top = t + 'px';
+    }
+});
+
+document.getElementById('left').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.left);
+    t = t - 10;
+    if(t > 0) {
+        document.getElementById('kenny').style.left = t + 'px';
+    }
+});
+
+document.getElementById('right').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.left);
+    t = t + 10;
+    if(t < 468){
+        document.getElementById('kenny').style.left = t + 'px';
+    }
+});
+
+document.getElementById('down').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.top);
+    t = t + 10;
+    if (t < 468)
     document.getElementById('kenny').style.top = t + 'px';
 });
